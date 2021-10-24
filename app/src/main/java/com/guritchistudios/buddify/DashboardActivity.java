888f53ee9b2c6027cb1cd495f6eafc0872281f7b
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-10-24 10:13:23 +0300

    Wire up the bottom nav to DashboardActivity, also set an action-bar with a title.

--- app/src/main/java/com/guritchistudios/buddify/DashboardActivity.java
+++ app/src/main/java/com/guritchistudios/buddify/DashboardActivity.java
@@ -1,14 +1,25 @@
 package com.guritchistudios.buddify;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class DashboardActivity extends AppCompatActivity {
+
+    ActionBar actionBar;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        actionBar = getSupportActionBar();
+        actionBar.setTitle("Account Activity");
+        bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setOnItemSelectedListener(selectedListener);
+
     }
 }
