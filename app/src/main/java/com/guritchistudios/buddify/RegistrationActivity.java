3792a97b1fe7fb2bb39ff10cd026203f76dc89f1
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-10-24 17:37:48 +0300

    Declare variables to hold view elements.

--- app/src/main/java/com/guritchistudios/buddify/RegistrationActivity.java
+++ app/src/main/java/com/guritchistudios/buddify/RegistrationActivity.java
@@ -2,13 +2,26 @@ package com.guritchistudios.buddify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText name, email, password;
+    private Button mRegister;
+    private TextView existingAccount;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
     }
 }
