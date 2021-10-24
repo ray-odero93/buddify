61b15bed274efd4c192c0a27c89c479aa4f6d230
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-10-24 17:46:37 +0300

    Wire up register button; set event listener, conditions to validate email address pattern and throw messages appropriately.

--- app/src/main/java/com/guritchistudios/buddify/RegistrationActivity.java
+++ app/src/main/java/com/guritchistudios/buddify/RegistrationActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -33,5 +34,27 @@ public class RegistrationActivity extends AppCompatActivity {
         password = findViewById(R.id.register_pass);
         mRegister = findViewById(R.id.register_button);
         existingAccount = findViewById(R.id.homepage);
+        mAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Register");
+
+        mRegister.setOnClickListener(view -> {
+            String userName = name.getText().toString().trim();
+            String userEmail = email.getText().toString().trim();
+            String userPass = password.getText().toString().trim();
+            if (!Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()) {
+                email.setError("Invalid e-mail address!");
+                email.setFocusable(true);
+            } else if (userPass.length() < 8) {
+                password.setError("Password length must be more than 8 characters.");
+                password.setFocusable(true);
+            } else {
+                registerUser(userName, userEmail, userPass);
+            }
+        });
+        
+    }
+
+    private void registerUser(String userName, String userEmail, String userPass) {
     }
 }
