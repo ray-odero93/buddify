98edf1cbab37d8763bd7a231841296430d644978
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-10-24 17:49:16 +0300

    Wire up existing account textView; set event listener on it,it will direct user to login page.

--- app/src/main/java/com/guritchistudios/buddify/RegistrationActivity.java
+++ app/src/main/java/com/guritchistudios/buddify/RegistrationActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -50,6 +52,13 @@ public class RegistrationActivity extends AppCompatActivity {
                 password.setFocusable(true);
             } else {
                 registerUser(userName, userEmail, userPass);
+            }
+        });
+
+        existingAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
             }
         });
         
