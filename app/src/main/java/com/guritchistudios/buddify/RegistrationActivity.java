48d440a81726ffe0492d361248f9e018471be72f
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-10-24 17:58:24 +0300

    Add an on failure function, along with message.

--- app/src/main/java/com/guritchistudios/buddify/RegistrationActivity.java
+++ app/src/main/java/com/guritchistudios/buddify/RegistrationActivity.java
@@ -1,18 +1,23 @@
 package com.guritchistudios.buddify;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -55,15 +60,41 @@ public class RegistrationActivity extends AppCompatActivity {
             }
         });
 
-        existingAccount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
-            }
-        });
-        
+        existingAccount.setOnClickListener(view -> startActivity(new Intent(RegistrationActivity.this, LoginActivity.class)));
     }
 
     private void registerUser(String userName, String userEmail, String userPass) {
+        progressDialog.show();
+        mAuth.createUserWithEmailAndPassword(userEmail, userPass).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                progressDialog.dismiss();
+                FirebaseUser user = mAuth.getCurrentUser();
+                assert user != null;
+                String email = user.getEmail();
+                String userId = user.getUid();
+                HashMap<Object, String> hashMap = new HashMap<>();
+                hashMap.put("email", email);
+                hashMap.put("userId", userId);
+                hashMap.put("name", userName);
+                hashMap.put("onlineStatus", "online");
+                hashMap.put("typingTo", "noOne");
+                hashMap.put("image", "");
+                FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+                DatabaseReference databaseReference = firebaseDatabase.getReference("Users");
+                databaseReference.child(userId).setValue(hashMap);
+                Toast.makeText(RegistrationActivity.this, "Registered User" + user.getEmail(), Toast.LENGTH_LONG).show();
+                Intent mainIntent = new Intent(RegistrationActivity.this, DashboardActivity.class);
+                mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(mainIntent);
+                finish();
+            } else {
+                progressDialog.dismiss();
+                Toast.makeText(RegistrationActivity.this, "Error", Toast.LENGTH_SHORT).show();
+            }
+        })
+                .addOnFailureListener(e -> {
+                    progressDialog.dismiss();
+                    Toast.makeText(RegistrationActivity.this, "Error occurred", Toast.LENGTH_LONG).show();
+                });
     }
 }
