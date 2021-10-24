ce568e922215a82d224ca5db0178c3f97980f9e6
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-10-24 11:59:55 +0300

    Declare variables to store view elements from display layout.

--- app/src/main/java/com/guritchistudios/buddify/LoginActivity.java
+++ app/src/main/java/com/guritchistudios/buddify/LoginActivity.java
@@ -1,14 +1,31 @@
 package com.guritchistudios.buddify;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText name, email, password;
+    private Button mLogin;
+    private TextView needNewAccount, recoveryPass;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setTitle("Create Account");
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
     }
 }
