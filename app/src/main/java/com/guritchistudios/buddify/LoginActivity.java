12e4705616370f0964a6e1fd38d8d536c4fc3063
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-10-24 17:08:02 +0300

    Define method to login user using e-mail address and password.

--- app/src/main/java/com/guritchistudios/buddify/LoginActivity.java
+++ app/src/main/java/com/guritchistudios/buddify/LoginActivity.java
@@ -7,14 +7,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import org.w3c.dom.Text;
+
+import java.util.HashMap;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -61,6 +71,40 @@ public class LoginActivity extends AppCompatActivity {
     private void loginUser(String userEmail, String userPass) {
         progressDialog.setMessage("Logging in...");
         progressDialog.show();
-
+        mAuth.signInWithEmailAndPassword(userEmail, userPass).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                progressDialog.dismiss();
+                FirebaseUser user = mAuth.getCurrentUser();
+                if (task.getResult().getAdditionalUserInfo().isNewUser()) {
+                    String userEmail1 = user.getEmail();
+                    String userId = user.getUid();
+                    HashMap<String, Object> hashMap = new HashMap<>();
+                    hashMap.put("email", userEmail1);
+                    hashMap.put("uid", userId);
+                    hashMap.put("name", "");
+                    hashMap.put("onLineStatus", "online");
+                    hashMap.put("typingTo", "noOne");
+                    hashMap.put("phone", "");
+                    hashMap.put("image", "");
+                    hashMap.put("cover", "");
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference reference = database.getReference("Users");
+                    reference.child(userId).setValue(hashMap);
+                }
+                assert user != null;
+                Toast.makeText(LoginActivity.this, "Registered user" + user.getEmail(), Toast.LENGTH_LONG).show();
+                Intent mainIntent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(mainIntent);
+                finish();
+            } else {
+                progressDialog.dismiss();
+                Toast.makeText(LoginActivity.this, "Login failed.", Toast.LENGTH_LONG).show();
+            }
+        })
+                .addOnFailureListener(e -> {
+                    progressDialog.dismiss();
+                    Toast.makeText(LoginActivity.this, "Error occurred.", Toast.LENGTH_SHORT).show();
+                });
     }
 }
