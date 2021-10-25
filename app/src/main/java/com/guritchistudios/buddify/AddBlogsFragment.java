6c0e1c43cc1c4f404118d31679babbb429fc8df0
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-10-25 08:53:01 +0300

    Define method to inflate the fragment layout and retrieve user data; name, e-mail, profile picture.

--- app/src/main/java/com/guritchistudios/buddify/AddBlogsFragment.java
+++ app/src/main/java/com/guritchistudios/buddify/AddBlogsFragment.java
@@ -1,9 +1,11 @@
 package com.guritchistudios.buddify;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -14,13 +16,14 @@ import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddBlogsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class AddBlogsFragment extends Fragment {
     public AddBlogsFragment() {
     }
@@ -42,4 +45,32 @@ public class AddBlogsFragment extends Fragment {
     DatabaseReference databaseReference;
     Button upload;
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        firebaseAuth = FirebaseAuth.getInstance();
+        View view = inflater.inflate(R.layout.fragment_add_blogs, container, false);
+        title = view.findViewById(R.id.ptitle);
+        des = view.findViewById(R.id.pdes);
+        image = view.findViewById(R.id.imagep);
+        upload = view.findViewById(R.id.pupload);
+        pd = new ProgressDialog(getContext());
+        pd.setCanceledOnTouchOutside(false);
+        Intent intent = getActivity().getIntent();
+        databaseReference = FirebaseDatabase.getInstance().getReference("Users");
+        Query query = databaseReference.orderByChild("email").equalTo(email);
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                    name = dataSnapshot1.child("name").getValue().toString();
+                    email = "" + dataSnapshot1.child("email").getValue();
+                    dp = "" + dataSnapshot1.child("image").getValue().toString();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 }
