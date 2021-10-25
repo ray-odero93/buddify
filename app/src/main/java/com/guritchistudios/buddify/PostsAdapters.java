eef79cdf6cad8ea4205ab201516ee86f0e37844b
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-10-25 15:15:42 +0300

    Wire up the view elements to custom recyclerView, MyHolder. This will display list of posts.

---
+++ app/src/main/java/com/guritchistudios/buddify/PostsAdapters.java
@@ -0,0 +1,170 @@
+package com.guritchistudios.buddify;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
+
+public class PostsAdapters extends RecyclerView.Adapter<PostsAdapters.MyHolder>{
+
+    List<PostsModel> postsModels;
+    Context context;
+    String myuid;
+    private DatabaseReference liekeref, postref;
+    boolean mprocesslike = false;
+
+    public PostsAdapters(Context context, List<PostsModel> postsModels) {
+        this.context = context;
+        this.postsModels = postsModels;
+        myuid = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+        liekeref = FirebaseDatabase.getInstance().getReference().child("likes");
+        postref = FirebaseDatabase.getInstance().getReference().child("posts");
+    }
+
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.row_posts, parent, false);
+        return new MyHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final MyHolder holder, @SuppressLint("RecyclerView") int position) {
+        final String uid = postsModels.get(position).getUid();
+        String nameh = postsModels.get(position).getUname();
+        final String titlee = postsModels.get(position).getTitle();
+        final String descri = postsModels.get(position).getDescription();
+        final String ptime = postsModels.get(position).getPtime();
+        String dp = postsModels.get(position).getUdp();
+        String plike = postsModels.get(position).getPlike();
+        final String image = postsModels.get(position).getUimage();
+        String email = postsModels.get(position).getUemail();
+        String comm = postsModels.get(position).getPcomments();
+        final String pid = postsModels.get(position).getPtime();
+        Calendar calendar = Calendar.getInstance(Locale.ENGLISH);
+        calendar.setTimeInMillis(Long.parseLong(ptime));
+        String timedate = DateFormat.format("dd/MM/yyyy hh:mm aa", calendar).toString();
+        holder.name.setText(nameh);
+        holder.title.setText(titlee);
+        holder.description.setText(descri);
+        holder.time.setText(timedate);
+        holder.like.setText(plike + " Likes");
+        holder.comments.setText(comm + " Comments");
+        setLikes(holder, ptime);
+        try {
+            Glide.with(context).load(dp).into(holder.picture);
+        } catch (Exception e) {
+
+        }
+        holder.image.setVisibility(View.VISIBLE);
+        try {
+            Glide.with(context).load(image).into(holder.image);
+        } catch (Exception e) {
+
+        }
+        holder.like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(holder.itemView.getContext(), PostLikedByActivity.class);
+                intent.putExtra("pid", pid);
+                holder.itemView.getContext().startActivity(intent);
+            }
+        });
+        holder.likebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final int plike = Integer.parseInt(postsModels.get(position).getPlike());
+                mprocesslike = true;
+                final String postid = postsModels.get(position).getPtime();
+                liekeref.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (mprocesslike) {
+                            if (dataSnapshot.child(postid).hasChild(myuid)) {
+                                postref.child(postid).child("plike").setValue("" + (plike - 1));
+                                liekeref.child(postid).child(myuid).removeValue();
+                                mprocesslike = false;
+                            } else {
+                                postref.child(postid).child("plike").setValue("" + (plike + 1));
+                                liekeref.child(postid).child(myuid).setValue("Liked");
+                                mprocesslike = false;
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+        holder.more.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showMoreOptions(holder.more, uid, myuid, ptime, image);
+            }
+        });
+        holder.comment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, PostDetailsActivity.class);
+                intent.putExtra("pid", ptime);
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return postsModels.size();
+    }
+
+    class MyHolder extends RecyclerView.ViewHolder {
+        ImageView picture, image;
+        TextView name, time, title, description, like, comments;
+        ImageButton more;
+        Button likebtn, comment;
+        LinearLayout profile;
+
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+            picture = itemView.findViewById(R.id.picturetv);
+            image = itemView.findViewById(R.id.pimagetv);
+            name = itemView.findViewById(R.id.unametv);
+            time = itemView.findViewById(R.id.utimetv);
+            more = itemView.findViewById(R.id.morebtn);
+            title = itemView.findViewById(R.id.ptitletv);
+            description = itemView.findViewById(R.id.descript);
+            like = itemView.findViewById(R.id.plikeb);
+            comments = itemView.findViewById(R.id.pcommentco);
+            likebtn = itemView.findViewById(R.id.like);
+            comment = itemView.findViewById(R.id.comment);
+            profile = itemView.findViewById(R.id.profilelayout);
+        }
+    }
+}
