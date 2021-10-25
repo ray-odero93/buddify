52c82e14803e8358f00fa51d0d5e464f4c02e298
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-10-25 16:41:56 +0300

    Define method to inflate the home fragment layout and set the data provided in the PostsAdapters file.

--- app/src/main/java/com/guritchistudios/buddify/HomeFragment.java
+++ app/src/main/java/com/guritchistudios/buddify/HomeFragment.java
@@ -3,6 +3,7 @@ package com.guritchistudios.buddify;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -11,13 +12,10 @@ import android.view.ViewGroup;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.ArrayList;
 import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class HomeFragment extends Fragment {
     FirebaseAuth firebaseAuth;
     String myuid;
@@ -27,4 +25,23 @@ public class HomeFragment extends Fragment {
 
     public HomeFragment() {
     }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        firebaseAuth = FirebaseAuth.getInstance();
+        recyclerView = view.findViewById(R.id.postrecyclerview);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(layoutManager);
+        posts = new ArrayList<>();
+        loadPosts();
+        return view;
+    }
+
+    private void loadPosts() {
+    }
+
 }
