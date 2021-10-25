b75277d0d81cb8b2642330c0ba90f003d8d9ad1b
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-10-25 07:35:32 +0300

    Complete user model logic.

--- app/src/main/java/com/guritchistudios/buddify/UsersModel.java
+++ app/src/main/java/com/guritchistudios/buddify/UsersModel.java
@@ -2,7 +2,69 @@ package com.guritchistudios.buddify;
 
 public class UsersModel {
     String name;
+    String onlineStatus;
+    String typingTo;
+    String email;
+    String image;
+    String uid;
 
     public UsersModel() {
+    }
+
+    public UsersModel(String name, String onlineStatus, String typingTo, String email, String image, String uid) {
+        this.name = name;
+        this.onlineStatus = onlineStatus;
+        this.typingTo = typingTo;
+        this.email = email;
+        this.image = image;
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOnlineStatus() {
+        return onlineStatus;
+    }
+
+    public void setOnlineStatus(String onlineStatus) {
+        this.onlineStatus = onlineStatus;
+    }
+
+    public String getTypingTo() {
+        return typingTo;
+    }
+
+    public void setTypingTo(String typingTo) {
+        this.typingTo = typingTo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
     }
 }
