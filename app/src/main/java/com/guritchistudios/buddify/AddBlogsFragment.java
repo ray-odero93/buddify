320088f0f7ff775421e618009e1c5e49aa657c70
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-10-25 08:48:52 +0300

    Declare properties/variables to hold view elements from layout file.

--- app/src/main/java/com/guritchistudios/buddify/AddBlogsFragment.java
+++ app/src/main/java/com/guritchistudios/buddify/AddBlogsFragment.java
@@ -1,5 +1,7 @@
 package com.guritchistudios.buddify;
 
+import android.app.ProgressDialog;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +9,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,51 +22,24 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class AddBlogsFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public AddBlogsFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddBlogsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddBlogsFragment newInstance(String param1, String param2) {
-        AddBlogsFragment fragment = new AddBlogsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    FirebaseAuth firebaseAuth;
+    EditText title, des;
+    private static final int CAMERA_REQUEST = 100;
+    private static final int STORAGE_REQUEST = 200;
+    String cameraPermission[];
+    String storagePermission[];
+    ProgressDialog pd;
+    ImageView image;
+    String edititle, editdes, editimage;
+    private static final int IMAGEPICK_GALLERY_REQUEST = 300;
+    private static final int IMAGE_PICKCAMERA_REQUEST = 400;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    Uri imageuri = null;
+    String name, email, uid, dp;
+    DatabaseReference databaseReference;
+    Button upload;
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_blogs, container, false);
-    }
 }
