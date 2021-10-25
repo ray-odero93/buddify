a2702f87eb066c4f8ee1d17e897086ec9299f0de
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-10-25 16:44:08 +0300

    Define method to retrieve posts data from database.

--- app/src/main/java/com/guritchistudios/buddify/HomeFragment.java
+++ app/src/main/java/com/guritchistudios/buddify/HomeFragment.java
@@ -2,6 +2,7 @@ package com.guritchistudios.buddify;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -9,8 +10,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,6 +49,24 @@ public class HomeFragment extends Fragment {
     }
 
     private void loadPosts() {
-    }
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Posts");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                posts.clear();
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                    PostsModel modelPost = dataSnapshot1.getValue(PostsModel.class);
+                    posts.add(modelPost);
+                    adapterPosts = new PostsAdapters(getActivity(), posts);
+                    recyclerView.setAdapter(adapterPosts);
+                }
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                Toast.makeText(getActivity(), databaseError.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 }
