4ddaaccfcd52d9d26eaf8eea4a580e567b8fae0e
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-10-26 12:15:43 +0300

    Complete adapter file for comments to enable retrieval of comments list.

--- app/src/main/java/com/guritchistudios/buddify/CommentsAdapter.java
+++ app/src/main/java/com/guritchistudios/buddify/CommentsAdapter.java
@@ -1,80 +1,86 @@
 package com.guritchistudios.buddify;
 
-public class CommentsAdapter {
-    public CommentsAdapter() {
+import android.content.Context;
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+public class CommentsAdapter extends RecyclerView.Adapter<CommentsAdapter.MyHolder> {
+
+
+    Context context;
+    List<CommentsModel> list;
+
+    public CommentsAdapter(Context context, List<CommentsModel> list, String myuid, String postid) {
+        this.context = context;
+        this.list = list;
+        this.myuid = myuid;
+        this.postid = postid;
     }
 
-    String cId;
-    String comment;
-    String ptime;
-    String udp;
-    String uid;
-    String uemail;
-    String uname;
+    String myuid;
+    String postid;
 
-    public CommentsAdapter(String cId, String comment, String ptime, String udp, String uid, String uemail, String uname) {
-        this.cId = cId;
-        this.comment = comment;
-        this.ptime = ptime;
-        this.udp = udp;
-        this.uid = uid;
-        this.uemail = uemail;
-        this.uname = uname;
+
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.row_comments, parent, false);
+        return new MyHolder(view);
     }
 
-    public String getcId() {
-        return cId;
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+        final String uid = list.get(position).getUid();
+        String name = list.get(position).getUname();
+        String email = list.get(position).getUemail();
+        String image = list.get(position).getUdp();
+        final String cid = list.get(position).getcId();
+        String comment = list.get(position).getComment();
+        String timestamp = list.get(position).getPtime();
+        Calendar calendar = Calendar.getInstance(Locale.ENGLISH);
+        calendar.setTimeInMillis(Long.parseLong(timestamp));
+        String timedate = DateFormat.format("dd/MM/yyyy hh:mm aa", calendar).toString();
+
+        holder.name.setText(name);
+        holder.time.setText(timedate);
+        holder.comment.setText(comment);
+        try {
+            Glide.with(context).load(image).into(holder.imagea);
+        } catch (Exception e) {
+
+        }
     }
 
-    public void setcId(String cId) {
-        this.cId = cId;
+    @Override
+    public int getItemCount() {
+        return list.size();
     }
 
-    public String getComment() {
-        return comment;
+    class MyHolder extends RecyclerView.ViewHolder {
+
+        ImageView imagea;
+        TextView name, comment, time;
+
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+            imagea = itemView.findViewById(R.id.loadcomment);
+            name = itemView.findViewById(R.id.commentname);
+            comment = itemView.findViewById(R.id.commenttext);
+            time = itemView.findViewById(R.id.commenttime);
+        }
     }
 
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    public String getPtime() {
-        return ptime;
-    }
-
-    public void setPtime(String ptime) {
-        this.ptime = ptime;
-    }
-
-    public String getUdp() {
-        return udp;
-    }
-
-    public void setUdp(String udp) {
-        this.udp = udp;
-    }
-
-    public String getUid() {
-        return uid;
-    }
-
-    public void setUid(String uid) {
-        this.uid = uid;
-    }
-
-    public String getUemail() {
-        return uemail;
-    }
-
-    public void setUemail(String uemail) {
-        this.uemail = uemail;
-    }
-
-    public String getUname() {
-        return uname;
-    }
-
-    public void setUname(String uname) {
-        this.uname = uname;
-    }
 }
