79ca08f7263154dd4250c60dbbca71bf40a741fb
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-10-26 11:58:59 +0300

    Declare properties.

--- app/src/main/java/com/guritchistudios/buddify/CommentsAdapter.java
+++ app/src/main/java/com/guritchistudios/buddify/CommentsAdapter.java
@@ -1,5 +1,17 @@
 package com.guritchistudios.buddify;
 
 public class CommentsAdapter {
+    public CommentsAdapter() {
+    }
+
+    String cId;
+    String comment;
+    String ptime;
+    String udp;
+    String uid;
+    String uemail;
+    String uname;
+
+
 
 }
