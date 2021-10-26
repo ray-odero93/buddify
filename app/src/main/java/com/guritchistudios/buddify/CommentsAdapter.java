8d7089a2ff349c0eb048568f6edd3c3036e3f74f
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-10-26 11:59:41 +0300

    Generate constructor method with properties.

--- app/src/main/java/com/guritchistudios/buddify/CommentsAdapter.java
+++ app/src/main/java/com/guritchistudios/buddify/CommentsAdapter.java
@@ -12,6 +12,13 @@ public class CommentsAdapter {
     String uemail;
     String uname;
 
-
-
+    public CommentsAdapter(String cId, String comment, String ptime, String udp, String uid, String uemail, String uname) {
+        this.cId = cId;
+        this.comment = comment;
+        this.ptime = ptime;
+        this.udp = udp;
+        this.uid = uid;
+        this.uemail = uemail;
+        this.uname = uname;
+    }
 }
