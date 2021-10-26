553afd9c6ca843e2f36eedb8b290bf78bfc1d9f3
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-10-26 12:05:00 +0300

    Generate constructor method with all properties.

--- app/src/main/java/com/guritchistudios/buddify/CommentsModel.java
+++ app/src/main/java/com/guritchistudios/buddify/CommentsModel.java
@@ -22,5 +22,59 @@ public class CommentsModel {
         this.uname = uname;
     }
 
+    public String getcId() {
+        return cId;
+    }
 
+    public void setcId(String cId) {
+        this.cId = cId;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getPtime() {
+        return ptime;
+    }
+
+    public void setPtime(String ptime) {
+        this.ptime = ptime;
+    }
+
+    public String getUdp() {
+        return udp;
+    }
+
+    public void setUdp(String udp) {
+        this.udp = udp;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getUemail() {
+        return uemail;
+    }
+
+    public void setUemail(String uemail) {
+        this.uemail = uemail;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
 }
