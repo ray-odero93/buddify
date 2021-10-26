3ca9204683a968f76429f345f3ee414f1c7c13ff
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-10-26 12:04:14 +0300

    Generate getter & setter for retrieving and changing data.

--- app/src/main/java/com/guritchistudios/buddify/CommentsModel.java
+++ app/src/main/java/com/guritchistudios/buddify/CommentsModel.java
@@ -11,4 +11,16 @@ public class CommentsModel {
     String uid;
     String uemail;
     String uname;
+
+    public CommentsModel(String cId, String comment, String ptime, String udp, String uid, String uemail, String uname) {
+        this.cId = cId;
+        this.comment = comment;
+        this.ptime = ptime;
+        this.udp = udp;
+        this.uid = uid;
+        this.uemail = uemail;
+        this.uname = uname;
+    }
+
+
 }
